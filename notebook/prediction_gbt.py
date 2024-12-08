fad6abcfbf2af9c193700670bccc59d5f27debbd
ali <ali_abde37@outlook.com>
2024-12-08 17:03:07 -0500

    added

--- notebook/prediction_gbt.py
+++ notebook/prediction_gbt.py
@@ -4,6 +4,7 @@ import sys
 from pyspark.sql import SparkSession
 from pyspark.ml import PipelineModel
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.feature import VectorAssembler, StandardScaler  # Add this import
 from io import StringIO
 import pandas as pd
 from pyspark.sql.functions import col
