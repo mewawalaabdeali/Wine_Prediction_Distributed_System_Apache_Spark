1fd6a8f943bc666ff2c34f42d918b41009b6ad96
ali <ali_abde37@outlook.com>
2024-12-09 22:10:04 -0500

    print

--- notebook/wine_xgb.py
+++ notebook/wine_xgb.py
@@ -153,6 +153,9 @@ print("-----------------------------------------------------------------------")
 print(f"[Test]  F1 Score      : {test_f1:.4f}")
 print(f"[Test]  Accuracy      : {test_accuracy:.4f}")
 print("***********************************************************************")
+print(f"Best model saved to: {model_dir}")
+print(f"MODEL_NAME={model_name}_{timestamp}")
+
 
 # Step 15: Stop Spark Session
 spark.stop()
